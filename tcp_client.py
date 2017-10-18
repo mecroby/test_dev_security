2ac3a326bc70a0036317d275c2ee9efdd578b85a
mecroby <mec.roby@gmail.com>
2017-10-18 19:32:15 -0400

    Add files via upload

---
+++ tcp_client.py
@@ -0,0 +1,16 @@
+#tcp client
+import socket
+
+target_host="www.google.com"
+target_port=80
+
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+client.connect((target_host,target_port))
+
+client.send("GET / HTTP/1.1\R\nHost: google.com\r\n\r\n")
+
+response=client.recv(4096)
+
+print response
+
